bcb6f129682f99db59d3e205f973a04d0b1d2c7e
nweber28 <70984558+nweber28@users.noreply.github.com>
2023-06-01 14:45:08 -0400

    Add FaceRecognition Comp

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Navigation from "./components/Navigation/Navigation";
+import FaceRecognition from "./components/FaceRecognition/FaceRecognition";
 import Logo from "./components/Logo/Logo";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
 import Rank from "./components/Rank/Rank";
@@ -7,52 +8,56 @@ import "./App.css";
 
 window.process = {};
 
-const Clarifai = require("clarifai");
+// const Clarifai = require("clarifai");
 
-const app = new Clarifai.App({
-  apiKey: "ab2787f7c4044364b006f9d240d50949",
-});
+// // const app = new Clarifai.App({
+// //   apiKey: "ab2787f7c4044364b006f9d240d50949",
+// // });
 
-const IMAGE_URL = "https://samples.clarifai.com/metro-north.jpg";
-
-const raw = JSON.stringify({
-  user_app_id: {
-    user_id: "clarifai",
-    app_id: "main",
-  },
-  inputs: [
-    {
-      data: {
-        image: {
-          url: IMAGE_URL,
-        },
-      },
-    },
-  ],
-});
-
-const requestOptions = {
-  method: "POST",
-  headers: {
-    Accept: "application/json",
-    Authorization: "Key 2a60cb99907e423ba396e2e67cd66442",
-  },
-  body: raw,
-};
+let IMAGE_URL = "";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
       input: "",
+      imageUrl: "",
     };
   }
 
   onInputChange = (event) => {
-    console.log(event.target.value);
+    this.setState({ input: event.target.value });
   };
 
   onButtonSubmit = () => {
+    this.setState({ imageUrl: this.state.input });
+    IMAGE_URL = this.state.input;
+
+    let raw = JSON.stringify({
+      user_app_id: {
+        user_id: "clarifai",
+        app_id: "main",
+      },
+      inputs: [
+        {
+          data: {
+            image: {
+              url: IMAGE_URL,
+            },
+          },
+        },
+      ],
+    });
+
+    let requestOptions = {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        Authorization: "Key 2a60cb99907e423ba396e2e67cd66442",
+      },
+      body: raw,
+    };
+
     fetch(
       `https://api.clarifai.com/v2/models/face-detection/versions/6dc7e46bc9124c5c8824be4822abe105/outputs`,
       requestOptions
@@ -63,7 +68,6 @@ class App extends Component {
       })
       .then((result) => console.log(result))
       .catch((error) => console.log("error", error));
-    console.log("click");
   };
 
   render() {
@@ -77,7 +81,7 @@ class App extends Component {
           onInputChange={this.onInputChange}
           onButtonSubmit={this.onButtonSubmit}
         />
-        {/*<FaceRecognition /> */}
+        <FaceRecognition imageUrl={this.state.imageUrl} />
       </div>
     );
   }
